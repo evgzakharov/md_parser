b6a7e45332c0519329b9eb4305ad62088b90af6e
Evgeny Zaharov <EZakharov2@alfabank.ru>
2018-06-15 10:59:03 +0300

    improve build scripts

--- js_build_and_run.sh
+++ js_build_and_run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash -ex
 
+RUN_FILE=${1:-'./files/file1.md'}
+
 ./gradlew :js_parser:clean :js_parser:build
 npm run --prefix js_parser build
-node js_parser/build/bundle/main.bundle.js ./files/file1.md
+node js_parser/build/bundle/main.bundle.js ${RUN_FILE}
--- jvm_build_and_run.sh
+++ jvm_build_and_run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash -ex
 
+RUN_FILE=${1:-'./files/file1.md'}
+
 ./gradlew :jvm_parser:clean :jvm_parser:shadowJar
-java -jar jvm_parser/build/libs/jvm_parser-all.jar ./files/file1.md
+java -jar jvm_parser/build/libs/jvm_parser-all.jar ${RUN_FILE}
