7e16afaeb772bbf47a9dc85cf27b6d1298a44d62
evgzakharov <evgzakharov88@gmail.com>
2018-06-15 14:13:58 +0300

    minor

--- js_build_and_run.sh
+++ js_build_and_run.sh
@@ -4,7 +4,9 @@ RUN_FILE=${1:-'./files/file1.md'}
 
 # build js code
 ./gradlew :js_parser:clean :js_parser:build
+
 # build js bundle
 npm run --prefix js_parser build
+
 # run bundle and pass file to parse as argument
 node js_parser/build/bundle/main.bundle.js ${RUN_FILE}
