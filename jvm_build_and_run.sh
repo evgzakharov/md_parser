89af0c155f5fd18e76a936b1c3aa555f3e4ea9e6
evgzakharov <evgzakharov88@gmail.com>
2018-06-15 11:06:28 +0300

    minor changes

--- jvm_build_and_run.sh
+++ jvm_build_and_run.sh
@@ -2,5 +2,7 @@
 
 RUN_FILE=${1:-'./files/file1.md'}
 
+# build applicatoin to jar
 ./gradlew :jvm_parser:clean :jvm_parser:shadowJar
+# run jar and pass file to parse as argument
 java -jar jvm_parser/build/libs/jvm_parser-all.jar ${RUN_FILE}
